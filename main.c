dc8e82138a969cf640d6f98482dce0b839e505a0
henryy13 <henrymarcel1539@gmail.com>
2023-02-11 22:16:55 -0800

    First commit

---
+++ main.c
@@ -0,0 +1,25 @@
+/*
+ * main.c
+ *
+ *  Created on: Feb. 11, 2023
+ *      Author: henry
+ */
+
+//define constants
+
+int main  (void)
+{
+//initialize clock
+//initialize serial communication
+//display initialization
+
+//constant operation
+while(1)
+{
+//get values from radar and compute speed
+//activate port pins to control LED board to show speed value
+//get values from pc
+//send values to pc
+}
+}
+
